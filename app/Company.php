de06ab3bdf19e195560efd5958fddc8854633bf3
Richard Ottinger <richard.ottinger@gmail.com>
2020-06-18 05:42:57 -0500

    Initial Laravel commit

---
+++ app/Company.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model {
+    public function users() {
+        return $this->hasMany('App\User');
+    }
+}
